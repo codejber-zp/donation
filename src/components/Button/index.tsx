3d9171322afb7bb6d923ca0bef218e7f850b2d8c
Ziemowit Przebitkowski <codejber@ziemowitprzebitkowski.pl>
2023-10-31 17:48:57 +0100

    feature: add button - TDD green (of course commits like this are only for recruiters ;) )

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,6 +3,6 @@ import React from "react";
 export interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
     text: string
 }
-export default function Button () {
-    return <button></button>
+export default function Button ({text}: ButtonProps) {
+    return <button>{text}</button>
 }
